2d685bdcbbe71fe7f11d320637bc0177ed223740
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-01-21 16:40:28 -0300

    first commit

--- nft/pages/index.tsx
+++ nft/pages/index.tsx
@@ -1,17 +1,20 @@
-import { Flex, Center, Text, Square, Box } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import {Flex, Center,Text, Square, Box} from "@chakra-ui/react"
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <Flex color='white'>
-    <Center w='100px' bg='green.500'>
-      <Text>Box 1</Text>
-    </Center>
-    <Square bg='blue.500' size='150px'>
-      <Text>Box 2</Text>
-    </Square>
-    <Box flex='1' bg='tomato'>
-      <Text>Box 3</Text>
-    </Box>
-  </Flex>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Square bg='blue.500' size='150px'>
+        <Text>Box 2</Text>
+      </Square>
+      <Box flex='1' bg='tomato'>
+        <Text>Box 3</Text>
+      </Box>
+    </Flex>
   )
 }
+
+export default Home
